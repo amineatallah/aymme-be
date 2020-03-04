4cee654782432653b3bc05eca2589f41dc7f0ca1
Amine <aminea@backbase.com>
2020-03-04 10:22:27 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const options = new DocumentBuilder()
+    .setTitle('AYMME API')
+    .setDescription('AYMME API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   app.enableCors();
   app.use(json({limit: '50mb'}));
   await app.listen(3000);
